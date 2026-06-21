273330e7a91241f96fd1991e9125f1a934a47246
Ashish Jha <ajha5645@gmail.com>
2026-06-21 12:06:09 +0530

    Add task scheduler core types

---
+++ src/main/java/com/taskscheduler/model/TaskStatus.java
@@ -0,0 +1,4 @@
+package com.taskscheduler.model;
+
+public enum TaskStatus {
+}
---
+++ src/main/java/com/taskscheduler/observer/ConsoleLogListener.java
@@ -0,0 +1,4 @@
+package com.taskscheduler.observer;
+
+public class ConsoleLogListener implements TaskEventListener {
+}
