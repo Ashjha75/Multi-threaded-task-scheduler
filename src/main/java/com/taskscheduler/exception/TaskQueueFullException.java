f43ce77f2d68f267057648713cfde941ea75f3a3
Ashish Jha <ajha5645@gmail.com>
2026-06-21 22:28:37 +0530

    added full working code for Task and Task  exceptions

--- src/main/java/com/taskscheduler/exception/TaskQueueFullException.java
+++ src/main/java/com/taskscheduler/exception/TaskQueueFullException.java
@@ -1,4 +1,22 @@
 package com.taskscheduler.exception;
 
-public class TaskQueueFullException extends TaskException {
+public class TaskQueueFullException extends RuntimeException {
+
+    private final int queueCapacity;
+    private final int tasksAttempted;
+
+    public TaskQueueFullException(String message, int queueCapacity, int tasksAttempted) {
+        super(message);
+        this.queueCapacity = queueCapacity;
+        this.tasksAttempted = tasksAttempted;
+    }
+
+    public int getTasksAttempted() {
+        return tasksAttempted;
+    }
+
+    public int getQueueCapacity() {
+        return queueCapacity;
+    }
+
 }
