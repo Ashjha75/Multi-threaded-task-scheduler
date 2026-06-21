02e5d6ca929972eee2a5f199613abce1b289a7e1
Ashish Jha <ajha5645@gmail.com>
2026-06-21 11:33:38 +0530

    Initialize Task Scheduler project

---
+++ src/main/java/com/taskscheduler/Main.java
@@ -0,0 +1,6 @@
+package com.taskscheduler;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/com/taskscheduler/command/AbstractTaskCommand.java
@@ -0,0 +1,4 @@
+package com.taskscheduler.command;
+
+public abstract class AbstractTaskCommand implements TaskCommand {
+}
