896b669720ef9b7545ad510fe5651f770c591115
Ashish Jha <ajha5645@gmail.com>
2026-06-22 17:37:27 +0530

    phase3 repository pattern

--- src/main/java/com/taskscheduler/repository/TaskRepository.java
+++ src/main/java/com/taskscheduler/repository/TaskRepository.java
@@ -1,4 +1,20 @@
 package com.taskscheduler.repository;
 
+import com.taskscheduler.model.Task;
+import com.taskscheduler.model.TaskPriority;
+import com.taskscheduler.model.TaskStatus;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface TaskRepository {
+    void save(Task task);
+    Optional<Task> findById(UUID id);
+    List<Task> findAll();
+    List<Task> findByStatus(TaskStatus status);
+    List<Task> findByPriority(TaskPriority priority);
+    void update(Task task);
+    boolean delete(UUID id);
+    int count();
 }
