b9e2ceebba4649e03fa5d5dc8b5ba39dca0e8ded
Ashish Jha <ajha5645@gmail.com>
2026-06-22 11:49:51 +0530

    Command DataCleanupCommand and NotificationCommand

--- src/main/java/com/taskscheduler/command/DataCleanupCommand.java
+++ src/main/java/com/taskscheduler/command/DataCleanupCommand.java
@@ -1,4 +1,26 @@
 package com.taskscheduler.command;
 
+import com.taskscheduler.model.Task;
+
 public class DataCleanupCommand extends AbstractTaskCommand {
+    private final String tableName;
+    private final int daysOld;
+
+    public DataCleanupCommand(Task task, String tableName, int daysOld) {
+        super(task);
+        this.tableName = tableName;
+        this.daysOld = daysOld;
+    }
+
+    @Override
+    protected void doExecute() throws Exception {
+        System.out.println("  [DataCleanup] Connecting to database...");
+        Thread.sleep(300);
+
+        System.out.println("  [DataCleanup] Running cleanup on table: " + tableName);
+        System.out.println("    Deleting records older than " + daysOld + " days");
+        Thread.sleep(800);  // Long-running query
+
+        System.out.println("  [DataCleanup] Deleted 1,245 records");
+    }
 }
--- src/main/java/com/taskscheduler/command/NotificationCommand.java
+++ src/main/java/com/taskscheduler/command/NotificationCommand.java
@@ -1,4 +1,21 @@
 package com.taskscheduler.command;
 
+import com.taskscheduler.model.Task;
+
 public class NotificationCommand extends AbstractTaskCommand {
+    private final String message;
+    private final String channel;  // "slack", "webhook", etc.
+    public NotificationCommand(Task task, String message, String channel) {
+        super(task);
+        this.message = message;
+        this.channel = channel;
+    }
+    @Override
+    protected void doExecute() throws Exception {
+        System.out.println("  [Notifier] Publishing to channel: " + channel);
+        System.out.println("    Message: " + message);
+        Thread.sleep(200);
+
+        System.out.println("  [Notifier] Notification delivered!");
+    }
 }
